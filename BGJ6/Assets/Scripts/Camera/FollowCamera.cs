426f21084020f9327e9020947a9fbec60ac2d6d4
Rahazan <guido.reaver@gmail.com>
2013-10-26 03:37:12 +0200

    Enemy and Player Speed fixed

--- BGJ6/Assets/Scripts/Camera/FollowCamera.cs
+++ BGJ6/Assets/Scripts/Camera/FollowCamera.cs
@@ -6,13 +6,20 @@ public class FollowCamera : MonoBehaviour {
     public Transform target;
 
 
+    private Vector3 targetPos;
+    private Vector3 startOffset;
+    private  Quaternion startRot;
+
+
 	// Use this for initialization
 	void Start () {
-	    
+        if (!target) target = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - target.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+
+        targetPos = target.position + startOffset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime);
 	}
 }
