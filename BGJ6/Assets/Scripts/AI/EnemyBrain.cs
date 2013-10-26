19bb050da17acb2b8d1e794d1c859b27c60b0142
Rahazan <guido.reaver@gmail.com>
2013-10-26 03:21:04 +0200

    Simple Enemy AI

--- BGJ6/Assets/Scripts/AI/EnemyBrain.cs
+++ BGJ6/Assets/Scripts/AI/EnemyBrain.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyBrain : MonoBehaviour {
 
-
+    GameObject target;
+    NavMeshAgent agent;
+    public float aggroDistance = 8f;
 
 
 	// Use this for initialization
 	void Start () {
-	
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (target && Vector3.Distance(target.transform.position, transform.position) < aggroDistance) 
+            MoveToTarget();
 	}
+
+    void MoveToTarget()
+    {
+        if (target != null)
+        {
+            if (Vector3.Distance(target.transform.position, transform.position) > 1.5f)
+            {
+                agent.enabled = true;
+                agent.SetDestination(target.transform.position);
+            }
+            else if (Vector3.Distance(target.transform.position, transform.position) < 1.25f)
+                agent.enabled = false;
+        }
+
+    }
+
 }
